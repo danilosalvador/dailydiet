5b869acc3e6142190f025deb5d25a1bc79a63737
Danilo Salvador <danilo.salvador@smartlogic.com.br>
2023-02-06 19:02:48 -0300

    Date and Time format functions

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -3,3 +3,11 @@ export const { format: formatPercent } = new Intl.NumberFormat(undefined, {
     minimumFractionDigits: 2,
     maximumFractionDigits: 2
 });
+
+export const { format: formatDate } = new Intl.DateTimeFormat(undefined, {
+    dateStyle: 'short',
+});
+
+export const { format: formatTime } = new Intl.DateTimeFormat(undefined, {
+    timeStyle: 'short',
+});
